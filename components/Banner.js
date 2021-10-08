a8504d17f1e2ef3ae002c9a619e1a08de099bb4c
thebosph <haciibrahimsimsek@ogr.iu.edu.tr>
2021-10-08 17:07:52 +0300

    Update Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
   return (
     <div>
       <div className="card bg-dark text-white shadow-lg">
-        <Image src={banner} class="card-img" />
+        <Image src={banner} className="card-img" />
         <div className="card-img-overlay mt-5 pt-5">
           <h5 className="card-title text-center">Rick And Morty</h5>
           <p className="card-text text-center">
