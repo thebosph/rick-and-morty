3296b1fe6eb0338b96b9f60aa7183cda64601ae1
thebosph <haciibrahimsimsek@ogr.iu.edu.tr>
2021-10-09 01:30:38 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,40 +10,13 @@ function MyApp({ Component, pageProps }) {
     import('bootstrap/dist/js/bootstrap')
   }, [])
 
-  const container = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        delayChildren: 0.5
-      }
-    }
-  }
-
-  const item = {
-    hidden: { opacity: 0 },
-    show: { opacity: 1 }
-  }
-
   return (
-    <AnimatePresence>
-      <div className="page-transition-wrapper">
-        <motion.div
-          variants={container}
-          initial="hidden"
-          animate="show"
-          id="page-transition-container"
-        >
-          <Head>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-          </Head>
-          <Component {...pageProps} />
-        </motion.div>
-      </div>
-    </AnimatePresence>
+    <div>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
   )
 }
 export default MyApp
